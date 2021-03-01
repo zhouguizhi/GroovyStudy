ad8b8046c1f65264ade8d23af2990efe7d5f38df
zhou.gz <2327322601@qq.com>
2021-03-01 19:26:21 +0800

    笔记

--- src/variable/VariableType.groovy
+++ src/variable/VariableType.groovy
@@ -1,16 +1,4 @@
 package variable
-
-//class VariableType {
-//
-//}
-//这是强类型 指定了是int数据类型
-int x = 100
-//会被包装成Integer
+int x = 9
 println(x.class)
-
-//弱类型定义一个变量 有点像js 这个是什么类型取决于赋值给他的变量是什么类型
-def y=10
-println(y.class)
-y="Hello World"
-println(y.class)
 
